fa645b22f44994c563dc93c9ee9155ef27ac5f9f
KhanhNguyenTRAN <khanh-nguyen.tran@epita.fr>
2025-01-16 12:34:09 +0100

    fix: Fix routing for Weather Api

--- app/src/main/java/com/tvt/weatherapp/data/remote/WeatherApi.kt
+++ app/src/main/java/com/tvt/weatherapp/data/remote/WeatherApi.kt
@@ -1,13 +1,29 @@
 package com.tvt.weatherapp.data.remote
 
-import com.tvt.weatherapp.data.model.UnsplashResponse
+import com.tvt.weatherapp.data.model.CurrentWeather
+import com.tvt.weatherapp.data.model.WeatherForecast
+import com.tvt.weatherapp.utils.Constants.OPENWEATHER_API_KEY
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface CityImageApi {
-    @GET("search/photos")
-    suspend fun getCityImage(
-        @Query("query") city: String,
-        @Query("client_id") apiKey: String
-    ): UnsplashResponse
+/**
+ * Interface for defining the OpenWeatherMap API endpoints.
+ */
+interface WeatherApi {
+
+    @GET("forecast")
+    suspend fun getWeatherForecast(
+        @Query("lat") latitude: Double,
+        @Query("lon") longitude: Double,
+        @Query("appid") apiKey: String = OPENWEATHER_API_KEY,
+        @Query("units") units: String = "metric"
+    ): WeatherForecast
+
+    @GET("weather")
+    suspend fun getCurrentWeather(
+        @Query("lat") latitude: Double,
+        @Query("lon") longitude: Double,
+        @Query("appid") apiKey: String = OPENWEATHER_API_KEY,
+        @Query("units") units: String = "metric"
+    ): CurrentWeather
 }
