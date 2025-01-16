8c05a64806213bfd091102e84fcfd514112c02ac
KhanhNguyenTRAN <khanh-nguyen.tran@epita.fr>
2025-01-16 13:26:17 +0100

    fix: Fix the theme and color declaration of the whole app

--- app/src/main/java/com/tvt/weatherapp/ui/theme/Color.kt
+++ app/src/main/java/com/tvt/weatherapp/ui/theme/Color.kt
@@ -2,10 +2,15 @@ package com.tvt.weatherapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Primary = Color(0xFF222A36)
+val Secondary = Color.White
+val Tertiary = Color(0xFF9396A6)
+val Background = Color(0xFF1B1D1E)
+val Surface = Color.White
+val Error = Color.Red
+val OnPrimary = Color.White
+val OnSecondary = Color.Black
+val OnTertiary = Color.Black
+val OnBackground = Color.Black
+val OnSurface = Color.Black
+val OnError = Color.White
